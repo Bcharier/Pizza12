a8b46c8cf47befd3b181d33fd0a1658373814a7d
Baptiste Charier <charier.ba@gmail.com>
2023-07-24 16:07:06 +0200

    initialize gradle project

---
+++ pizza12/src/main/java/fr/eni/pizza12/Pizza12Application.java
@@ -0,0 +1,13 @@
+package fr.eni.pizza12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pizza12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pizza12Application.class, args);
+	}
+
+}
---
+++ pizza12/src/test/java/fr/eni/pizza12/Pizza12ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.pizza12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pizza12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
