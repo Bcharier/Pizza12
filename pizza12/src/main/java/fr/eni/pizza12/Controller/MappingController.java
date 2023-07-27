7b70b6dada163b9aac5166374bc1356632599663
Baptiste Charier <charier.ba@gmail.com>
2023-07-27 12:06:32 +0200

    création des pages

---
+++ pizza12/src/main/java/fr/eni/pizza12/Controller/MappingController.java
@@ -0,0 +1,48 @@
+package fr.eni.pizza12.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MappingController {
+    @GetMapping("/")
+    public String helloPizza12() {
+        return "index";
+    }
+
+    @GetMapping("/designSystem")
+    public String designSystem() {
+        return "designSystem";
+    }
+
+    @GetMapping("/account")
+    public String openAccount() {
+        return "account";
+    }
+
+    @GetMapping("/cart")
+    public String openCart() {
+        return "cart";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/employeeRedirect")
+    public String serviceTools() {
+        return "serviceTools";
+    }
+
+    @GetMapping("/listOrders")
+    public String listOrders() {
+        return "listOrders";
+    }
+
+    @GetMapping("orderTool")
+    public String orderTool() {
+        return "orderTool";
+    }
+}
