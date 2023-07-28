41076c585bf897aa90b16e046f5570a1e5e5c0ab
Baptiste Charier <charier.ba@gmail.com>
2023-07-28 10:08:37 +0200

    bug fix AccountRepositoryImpl

---
+++ pizza12/src/main/java/fr/eni/pizza12/dal/AccountRepositoryImpl.java
@@ -0,0 +1,44 @@
+// package fr.eni.pizza12.dal;
+
+// import java.util.List;
+
+// import fr.eni.pizza12.bo.AccountEntity;
+
+// public class AccountRepositoryImpl implements AccountRepository {
+
+// @Override
+// public List<AccountEntity> getAllAccounts() {
+// // TODO Auto-generated method stub
+// throw new UnsupportedOperationException("Unimplemented method
+// 'getAllAccounts'");
+// }
+
+// @Override
+// public AccountEntity getAccountbyId(int id) {
+// // TODO Auto-generated method stub
+// throw new UnsupportedOperationException("Unimplemented method
+// 'getAccountbyId'");
+// }
+
+// @Override
+// public List<AccountEntity> getAccountByFirstName(String accountFirstName) {
+// // TODO Auto-generated method stub
+// throw new UnsupportedOperationException("Unimplemented method
+// 'getAccountByFirstName'");
+// }
+
+// @Override
+// public List<AccountEntity> getAccountByLastName(String accountLastName) {
+// // TODO Auto-generated method stub
+// throw new UnsupportedOperationException("Unimplemented method
+// 'getAccountByLastName'");
+// }
+
+// @Override
+// public List<AccountEntity> getAccountByName(String accountName) {
+// // TODO Auto-generated method stub
+// throw new UnsupportedOperationException("Unimplemented method
+// 'getAccountByName'");
+// }
+
+// }
