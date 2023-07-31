47a2913b34464c6edb3080bfcb59cb0a54fcb35b
Baptiste Charier <charier.ba@gmail.com>
2023-07-31 12:06:14 +0200

    controller fix

---
+++ pizza12/src/main/java/fr/eni/pizza12/controller/RESTController.java
@@ -0,0 +1,23 @@
+package fr.eni.pizza12.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.eni.pizza12.bo.OrderEntity;
+import fr.eni.pizza12.bo.OrderItemEntity;
+
+// @RestController
+// public class RESTController {
+
+// @PostMapping
+// public OrderEntity method(@RequestBody OrderItemEntity orderItemEntity) {
+// return
+// }
+
+// @PostMapping
+// public ResponseEntity<OrderEntity> method() {
+// return ResponseEntity
+// }
+// }
